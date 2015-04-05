4c23f782f6fd31721d8ed018b3ca68d962e52cb5
Kathryn Manning <manningkathryn@gmail.com>
2015-04-05 15:05:14 -0600

    Initial Commit

---
+++ Numo_0.0Tests/Numo_0_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Numo_0_0Tests.swift
+//  Numo_0.0Tests
+//
+//  Created by Kathryn Manning on 4/5/15.
+//  Copyright (c) 2015 kathrynmanning. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Numo_0_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
